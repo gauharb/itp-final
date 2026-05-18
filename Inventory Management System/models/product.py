69aa2054c09d12745a91956ea86797804c858055
gauharb <gauhara.baimagambet@gmail.com>
2026-05-18 21:21:02 +0500

    Properties manage product data.Getter return values like name,price,quantity while setters validate input before saving

--- Inventory Management System/models/product.py
+++ Inventory Management System/models/product.py
@@ -25,3 +25,49 @@ class Product:
     @property
     def description(self):
         return self._description
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not value.strip():
+            raise ValueError("Name cannot be empty")
+        self._name = value.strip()
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self._price = value
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        if value < 0:
+            raise ValueError("Quantity cannot be negative")
+        self._quantity = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        self._category = value.strip().lower()
+
+    @property
+    def expiry_date(self):
+        return self._expiry_date
+
+    @expiry_date.setter
+    def expiry_date(self, value):
+        self._expiry_date = value
