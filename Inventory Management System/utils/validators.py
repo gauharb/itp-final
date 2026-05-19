d5a298d9fe38f0a1fab614f5177df3cd15870fb5
mukhtrvaad <mdana7754@gmail.com>
2026-05-19 23:09:15 +0500

    Add basic validation functions

---
+++ Inventory Management System/utils/validators.py
@@ -0,0 +1,17 @@
+import re
+
+
+# Regular expressions to verify format
+
+_DATE_PATTERN = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+_PRICE_PATTERN = re.compile(r"^\d+(\.\d{1,2})?$")
+
+
+def is_valid_date(value):
+    """Checks the date format YYYY-MM-DD"""
+    return bool(_DATE_PATTERN.match(str(value)))
+
+
+def is_valid_price(value):
+    """Checks that the price is a number with a maximum of 2 decimal places"""
+    return bool(_PRICE_PATTERN.match(str(value)))
